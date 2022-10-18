3ef068aeee2397020a70adb3f4c4183536cf4c3e
Rafael Barbosa <rafaeldacostabarbosa@gmail.com>
2022-10-18 20:24:27 -0300

    adiciona envs ao projeto

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 module.exports = {
   "development": {
-    "username": "admin",
-    "password": 123456,
-    "database": "auth-db",
-    "host": "localhost",
-    "dialect": "postgres",
-    "port": 5433,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.DB_PORT,
     "define": {
       "timestamps": true,
       "underscored": true,
--- src/server.js
+++ src/server.js
@@ -1,24 +1,14 @@
 const express = require("express");
-require('./database/index');
-
-const User = require("./models/User.js");
-
 const userRoutes = require("./routes/UserRoutes.js");
+
+require('dotenv').config();
+require('./database/index');
 
 const app = express();
 app.use(express.json());
 app.use(userRoutes);
 
-const env = process.env;
-const PORT = env.PORT || 3000;
-
-app.get('/api/status', (req, res) => {
-    return res.status(200).json({
-        service: 'AUTH-API',
-        status: 'up',
-        httpStatus: 200
-    });
-});
+const PORT = process.env.API_PORT || 3000;
 
 app.listen(PORT, () => {
     console.info(`Server running on port ${PORT}`);
